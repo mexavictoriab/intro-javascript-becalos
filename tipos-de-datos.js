698921b0c8350a2fcc17405567e11b2e7edf0d68
mexavictoriab <victoryabvi@gmail.com>
2025-02-05 20:49:59 -0600

    actividad sesión 1

---
+++ tipos-de-datos.js
@@ -0,0 +1,41 @@
+/*Tipos de datos*/
+
+// ---> NUMEROS
+
+let a = 30
+let b = 12
+let suma = a + b
+
+console.log(suma);
+console.log(typeof suma);
+console.log('-----');
+
+let sumaConLetra = 'Cuarenta y dos'
+
+console.log(sumaConLetra);
+console.log(typeof sumaConLetra);
+console.log('-----');
+
+let c = -696
+let resta = a - c
+
+console.log(resta);
+console.log(typeof resta);
+console.log('-----');
+
+let estaLogueado = true
+
+console.log(estaLogueado);
+console.log(typeof estaLogueado);
+console.log('-----');
+
+let sinValor;
+
+console.log(sinValor);
+console.log(typeof sinValor);
+console.log('-----');
+
+let resultadoInvalido = "Hola" * 10
+
+console.log(resultadoInvalido);
+console.log(typeof resultadoInvalido);
